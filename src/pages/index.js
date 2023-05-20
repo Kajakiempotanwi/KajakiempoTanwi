9976dcf4f2ddca9da69e4e15707f27f073ffd636
Valkon <96625039+Nawija@users.noreply.github.com>
2023-05-20 09:32:08 +0200

    d
    d

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ import { StaticImage } from "gatsby-plugin-image";
 const Home = () => {
     return (
         <Layout>
-            <div className="w-full min-h-[25rem] relative">
+            <div className="w-full min-h-[20rem] relative">
                 <StaticImage
                     quality={100}
-                    className="w-full h-full min-h-[25rem] object-right"
+                    className="w-full h-full min-h-[20rem] object-right"
                     alt="Kajaki Po Tanwi"
                     src="../assets/hero.jpg"
                 />
