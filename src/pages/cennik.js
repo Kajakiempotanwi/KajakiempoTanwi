2b9346c6ce6a57e55adb99d8632e20487d2972de
Valkon <96625039+Nawija@users.noreply.github.com>
2023-05-25 17:34:14 +0200

    Update cennik.js

--- src/pages/cennik.js
+++ src/pages/cennik.js
@@ -8,14 +8,18 @@ const Cennik = () => {
     return (
         <Layout>
             <section id="hero" className="w-full min-h-[15rem] relative">
-                <StaticImage
-                    quality={100}
-                    layout="constrained"
-                    loading="eager"
-                    className="w-full max-h-[15rem] min-h-[15rem] bg-fixed"
-                    alt="Kajaki Po Tanwi"
-                    src="../assets/hero.jpg"
-                />
+                <div
+                    className="w-full max-h-[15rem] min-h-[15rem] bg-fixed overflow-hidden"
+                    style={{ objectPosition: "top" }}
+                >
+                    <StaticImage
+                        quality={100}
+                        layout="fullWidth"
+                        loading="eager"
+                        alt="Kajakiem po Tanwi Spływy kajakowe Susiec"
+                        src="../assets/hero.jpg"
+                    />
+                </div>
                 <div className="absolute w-full h-full inset-0 opacity-50 bg-[#00232E]" />
                 <div className="absolute w-full left-1/2 top-1/2 px-4 -translate-x-1/2 -translate-y-1/2">
                     <h1 className="uppercase orange_gradient mt-12 text-xl sm:text-2xl md:text-3xl  lg:text-5xl font-bold -tracking-wide">
